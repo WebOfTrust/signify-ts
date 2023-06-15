7acd3dc696eaf394e85ebcdf412c716be37c37f5
Kevin Griffin <griffin.kev@gmail.com>
2023-06-15 16:28:15 -0400

    dater

--- test/core/diger.test.ts
+++ test/core/diger.test.ts
@@ -1,4 +1,4 @@
-import { MatterCodex, matter_codex_code } from "../../pkg/cesride_wasm";
+import { Dater, MatterCodex, matter_codex_code } from "../../pkg/cesride_wasm";
 // import { createHash } from "blake3"
 // import { strict as assert } from "assert";
 // import { Matter as Tables } from "../../src/keri/core/matter";
@@ -8,6 +8,17 @@ describe('Diger', () => {
   it('should generate digests', () => {
     console.log(MatterCodex.Blake2b_256);
     console.log(matter_codex_code(MatterCodex.Blake2b_256));
+
+    const date = Dater.new_with_dts("2020-08-22T17:50:09.988921+00:00");
+    console.log("<p>Date:</p>");
+    console.log("dts: " + date.dts() + "<br/>");
+    console.log("dtsb: " + date.dtsb() + "<br/>");
+    console.log("code: " + date.code() + "<br/>");
+    console.log("size: " + date.size() + "<br/>");
+    console.log("raw: " + date.raw() + "<br/>");
+    console.log("qb64: " + date.qb64() + "<br/>");
+    console.log("qb64b: " + date.qb64b() + "<br/>");
+    console.log("qb2: " + date.qb2() + "<br/>");
 
     // let diger = Diger.new_with_qb64("ELC5L3iBVD77d_MYbYGGCUQgqQBju1o4x1Ud");
     // console.log(diger);
