7d05dd39f0ee19548a3cdabbf1aed3dcfb56cb37
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-09-27 14:40:13 -0400

    ipex part duex

--- src/keri/app/credentialing.ts
+++ src/keri/app/credentialing.ts
@@ -1,14 +1,16 @@
 import { SignifyClient } from "./clienting"
-import {Salter} from "../core/salter"
-import { interact, messagize} from "../core/eventing"
-import {b, Dict, Ident, Ilks, Serials, versify} from "../core/core"
-import {MtrDex} from "../core/matter"
-import {Saider} from "../core/saider"
-import {Serder} from "../core/serder"
-import {Siger} from "../core/siger"
-import {Prefixer} from "../core/prefixer"
-import {randomNonce} from "./coring"
-import {TextDecoder} from "util"
+import { Salter } from "../core/salter"
+import { interact, issue } from "../core/eventing"
+import { b, Dict, Ident, Ilks, Serials, versify } from "../core/core"
+import { MtrDex } from "../core/matter"
+import { Saider } from "../core/saider"
+import { Serder } from "../core/serder"
+import { Siger } from "../core/siger"
+import { Prefixer } from "../core/prefixer"
+import { randomNonce } from "./coring"
+import { TextDecoder } from "util"
+import { Habery } from "./habery"
+import { SEMANTIC_TYPES } from "cbor/types/lib/encoder"
 
 
 /** Types of credentials */
@@ -19,9 +21,9 @@ export class CredentialTypes {
 
 /** Credential filter parameters */
 export interface CredentialFilter {
-    filter?: object, 
-    sort?: object[], 
-    skip?: number, 
+    filter?: object,
+    sort?: object[],
+    skip?: number,
     limit?: number
 }
 
@@ -45,7 +47,7 @@ export class Credentials {
      * @param {CredentialFilter} [kargs] Optional parameters to filter the credentials
      * @returns {Promise<any>} A promise to the list of credentials
      */
-    async list(name: string, kargs:CredentialFilter ={}): Promise<any> {
+    async list(name: string, kargs: CredentialFilter = {}): Promise<any> {
         let path = `/identifiers/${name}/credentials/query`
         let filtr = kargs.filter === undefined ? {} : kargs.filter;
         let sort = kargs.sort === undefined ? [] : kargs.sort;
@@ -75,26 +77,27 @@ export class Credentials {
     async get(name: string, said: string, includeCESR: boolean = false): Promise<any> {
         let path = `/identifiers/${name}/credentials/${said}`
         let method = 'GET'
-        let headers = includeCESR? new Headers({'Accept': 'application/json+cesr'}) : new Headers({'Accept': 'application/json'})
+        let headers = includeCESR ? new Headers({ 'Accept': 'application/json+cesr' }) : new Headers({ 'Accept': 'application/json' })
         let res = await this.client.fetch(path, method, null, headers)
 
-        return includeCESR? await res.text() : await res.json()
+        return includeCESR ? await res.text() : await res.json()
     }
 
-    /**
-     * Issue a credential
-     * @async
-     * @param {string} name Name or alias of the identifier
-     * @param {string} registy qb64 AID of credential registry
-     * @param {string} schema SAID of the schema
-     * @param {string} [recipient] Optional prefix of recipient identifier
-     * @param {any} [credentialData] Optional credential data
-     * @param {any} [rules] Optional credential rules
-     * @param {any} [source] Optional credential sources
-     * @param {boolean} [priv=false] Flag to issue a credential with privacy preserving features
-     * @returns {Promise<any>} A promise to the long-running operation
-     */
-    async issue(name: string, registy: string, schema: string, recipient?: string, credentialData?: any, rules?: any, source?: any, priv: boolean=false): Promise<any> {
+    /** 
+    * Create a credential
+    * @async
+    * @param {any} name - Identifier
+    * @param {string} registry - Registry
+    * @param {Dict<any>} credentialData - Data for the credential
+    * @param string schema - Schema
+    * @param {string} [recipient] - Recipient of the credential
+    * @param {Dict<any>} [edges] - Edges for the credential
+    * @param {Dict<any>} [rules] - Rules for the credential
+    * @param {boolean} [priv] - Private credential
+    * @param {string} [timestamp] - Timestamp for the credential
+    */
+
+    async create(name: any, registry: string, credentialData: Dict<any>, schema: string, recipient: string | undefined, edges: Dict<any>, rules: Dict<any>, priv: boolean, timestamp: string) {
         // Create Credential
         let hab = await this.client.identifiers().get(name)
         let pre: string = hab.prefix
@@ -118,16 +121,16 @@ export class Credentials {
             subject.i = recipient
         }
         subject.dt = dt
-        subject = {...subject, ...credentialData}
+        subject = { ...subject, ...credentialData }
 
-        const [, a] = Saider.saidify(subject,undefined,undefined,"d")
+        const [, a] = Saider.saidify(subject, undefined, undefined, "d")
 
-        cred = {...cred, i:pre}
-        cred.ri = registy
-        cred = {...cred,...{s: schema}, ...{a: a}}
+        cred = { ...cred, i: pre }
+        cred.ri = registry
+        cred = { ...cred, ...{ s: schema }, ...{ a: a } }
 
-        if (source !== undefined ) {
-            cred.e = source
+        if (edges !== undefined) {
+            cred.e = edges
         }
         if (rules !== undefined) {
             cred.r = rules
@@ -141,7 +144,7 @@ export class Credentials {
             d: "",
             i: vc.d,
             s: "0",
-            ri: registy,
+            ri: registry,
             dt: dt
 
         }
@@ -149,13 +152,12 @@ export class Credentials {
         let [, iss] = Saider.saidify(_iss)
 
         // Create paths and sign
-        let cpath = '6AABAAA-'
         let keeper = this.client!.manager!.get(hab)
-        let csigs = keeper.sign(b(JSON.stringify(vc)))
 
         // Create ixn
         let ixn = {}
         let sigs = []
+        let serder = undefined
 
         let state = hab.state
         if (state.c !== undefined && state.c.includes("EO")) {
@@ -167,7 +169,7 @@ export class Credentials {
         let sn = Number(state.s)
         let dig = state.d
 
-        let data:any = [{
+        let data: any = [{
             i: iss.i,
             s: iss.s,
             d: iss.d
@@ -178,232 +180,64 @@ export class Credentials {
             throw new Error("Establishment only not implemented")
 
         } else {
-            let serder = interact({ pre: pre, sn: sn + 1, data: data, dig: dig, version: undefined, kind: undefined })
+            serder = interact({ pre: pre, sn: sn + 1, data: data, dig: dig, version: undefined, kind: undefined })
             sigs = keeper.sign(b(serder.raw))
             ixn = serder.ked
         }
+        let res = await this.createFromEvents(name, vc, iss.ked, ixn, sigs)
 
-        let body = {
-            cred: vc,
-            csigs: csigs,
-            path: cpath,
-            iss: iss,
-            ixn: ixn,
-            sigs: sigs
-        }
-
-        let path = `/identifiers/${name}/credentials`
-        let method = 'POST'
-        let headers = new Headers({
-            'Accept': 'application/json+cesr'
-
-        })
-        let res = await this.client.fetch(path, method, body, headers)
-        return await res.json()
+        return [vc, iss, ixn, sigs, res.json()]
 
     }
 
-    /**
-     * Revoke credential
-     * @async
-     * @param {string} name Name or alias of the identifier
-     * @param {string} said SAID of the credential
-     * @returns {Promise<any>} A promise to the long-running operation
-     */
-    async revoke(name: string, said: string): Promise<any> {
-        let hab = await this.client.identifiers().get(name)
-        let pre: string = hab.prefix
-
-        const vs = versify(Ident.KERI, undefined, Serials.JSON, 0)
-        const dt = new Date().toISOString().replace('Z', '000+00:00')
-
-        let cred = await this.get(name, said)
-
-        // Create rev
-        let _rev = {
-            v: vs,
-            t: Ilks.rev,
-            d: "",
-            i: said,
-            s: "1",
-            p: cred.status.d,
-            ri: cred.sad.ri,
-            dt: dt
-        }
-
-        let [, rev] = Saider.saidify(_rev)
-
-        // create ixn
-        let ixn = {}
-        let sigs = []
-
-        let state = hab.state
-        if (state.c !== undefined && state.c.includes("EO")) {
-            var estOnly = true
-        }
-        else {
-            var estOnly = false
-        }
-
-        let sn = Number(state.s)
-        let dig = state.d
-
-        let data:any = [{
-            i: rev.i,
-            s: rev.s,
-            d: rev.d
-        }]
-        if (estOnly) {
-            // TODO implement rotation event
-            throw new Error("Establishment only not implemented")
-
-        } else {
-            let serder = interact({ pre: pre, sn: sn + 1, data: data, dig: dig, version: undefined, kind: undefined })
-            let keeper = this.client!.manager!.get(hab)
-            sigs = keeper.sign(b(serder.raw))
-            ixn = serder.ked
-        }
-
+    async createFromEvents(name: string, creder: Dict<any>, iss: Dict<any>, anc: Dict<any>, sigs: any[]) {
         let body = {
-            rev: rev,
-            ixn: ixn,
+            acdc: creder,
+            iss: iss,
+            ixn: anc,
             sigs: sigs
         }
 
-        let path = `/identifiers/${name}/credentials/${said}`
+        let path = `/identifiers/${name}/credentials/`
         let method = 'DELETE'
         let headers = new Headers({
-            'Accept': 'application/json+cesr'
-
+            'Accept': 'application/json'
         })
         let res = await this.client.fetch(path, method, body, headers)
         return await res.json()
 
     }
+}
 
+export class Ipex {
+    public client: SignifyClient
     /**
-     * Present a credential
-     * @async
-     * @param {string} name Name or alias of the identifier
-     * @param {string} said SAID of the credential
-     * @param {string} recipient Identifier prefix of the receiver of the presentation
-     * @param {boolean} [include=true] Flag to indicate whether to stream credential alongside presentation exchange message
-     * @returns {Promise<string>} A promise to the long-running operation
+     * Ipex
+     * @param {SignifyClient} client 
      */
-    async present(name: string, said: string, recipient: string, include: boolean=true): Promise<string> {
+    constructor(client: SignifyClient) {
+        this.client = client
+    }
 
+    async grant(name: string, recp: string, message: string, acdc: any, iss: any, anc: any, date: string) {
+        let exchanges = this.client.exchanges()
         let hab = await this.client.identifiers().get(name)
-        let pre: string = hab.prefix
+        let sender = hab.prefix
 
-        let cred = await this.get(name, said)
         let data = {
-            i: cred.sad.i,
-            s: cred.sad.s,
-            n: said
+            m: message,
+            i: recp
         }
-
-        const vs = versify(Ident.KERI, undefined, Serials.JSON, 0)
-
-        const _sad = {
-            v: vs,
-            t: Ilks.exn,
-            d: "",
-            dt: new Date().toISOString().replace("Z","000+00:00"),
-            r: "/presentation",
-            q: {},
-            a: data
+        let embeds = {
+            acdc: acdc,
+            iss: iss,
+            anc: anc
         }
-        const [, sad] = Saider.saidify(_sad)
-        const exn = new Serder(sad)
+        let [grant, gsigs, end] = exchanges.createExchangeMessage(sender, "/ipex/grant", data, embeds, date)
 
-        let keeper = this.client!.manager!.get(hab)
-
-        let sig = keeper.sign(b(exn.raw),true)
-
-        let siger = new Siger({qb64:sig[0]})
-        let seal = ["SealLast" , {i:pre}]
-        let ims = messagize(exn,[siger],seal, undefined, undefined, true)
-        ims = ims.slice(JSON.stringify(exn.ked).length)
-
-
-        let body = {
-            exn: exn.ked,
-            sig: new TextDecoder().decode(ims),
-            recipient: recipient,
-            include: include
-        }
-
-        let path = `/identifiers/${name}/credentials/${said}/presentations`
-        let method = 'POST'
-        let headers = new Headers({
-            'Accept': 'application/json+cesr'
-
-        })
-        let res = await this.client.fetch(path, method, body, headers)
-        return await res.text()
-
+        return [grant, gsigs, end]
     }
 
-    /**
-     * Request a presentation of a credential
-     * @async
-     * @param {string} name Name or alias of the identifier
-     * @param {string} recipient Identifier prefix of the receiver of the presentation
-     * @param {string} schema SAID of the schema
-     * @param {string} [issuer] Optional prefix of the issuer of the credential
-     * @returns {Promise<string>} A promise to the long-running operation
-     */
-    async request(name: string, recipient: string, schema: string, issuer?: string): Promise<string> {
-        let hab = await this.client.identifiers().get(name)
-        let pre: string = hab.prefix
-
-        let data:any = {
-            s: schema
-        }
-        if (issuer !== undefined) {
-            data["i"] = issuer
-        }
-
-        const vs = versify(Ident.KERI, undefined, Serials.JSON, 0)
-
-        const _sad = {
-            v: vs,
-            t: Ilks.exn,
-            d: "",
-            dt: new Date().toISOString().replace("Z","000+00:00"),
-            r: "/presentation/request",
-            q: {},
-            a: data
-        }
-        const [, sad] = Saider.saidify(_sad)
-        const exn = new Serder(sad)
-
-        let keeper = this.client!.manager!.get(hab)
-
-        let sig = keeper.sign(b(exn.raw),true)
-
-        let siger = new Siger({qb64:sig[0]})
-        let seal = ["SealLast" , {i:pre}]
-        let ims = messagize(exn,[siger],seal, undefined, undefined, true)
-        ims = ims.slice(JSON.stringify(exn.ked).length)
-
-
-        let body = {
-            exn: exn.ked,
-            sig: new TextDecoder().decode(ims),
-            recipient: recipient,
-        }
-
-        let path = `/identifiers/${name}/requests`
-        let method = 'POST'
-        let headers = new Headers({
-            'Accept': 'application/json+cesr'
-
-        })
-        let res = await this.client.fetch(path, method, body, headers)
-        return await res.text()
-
-    }
 }
 
 /**
@@ -425,7 +259,7 @@ export class Registries {
      * @param {string} name Name or alias of the identifier
      * @returns {Promise<any>} A promise to the list of registries
      */
-    async list(name:string): Promise<any> {
+    async list(name: string): Promise<any> {
         let path = `/identifiers/${name}/registries`
         let method = 'GET'
         let res = await this.client.fetch(path, method, null)
@@ -441,7 +275,7 @@ export class Registries {
      * @param {string} [nonce] Nonce used to generate the registry. If not provided a random nonce will be generated
      * @returns {Promise<any>} A promise to the long-running operation
      */
-    async create(name: string, registryName: string, nonce?:string): Promise<any> {
+    async create(name: string, registryName: string, nonce?: string): Promise<any> {
         // TODO add backers option
         // TODO get estOnly from get_identifier ?
 
@@ -464,7 +298,7 @@ export class Registries {
             n: nonce
         }
 
-        let prefixer = new Prefixer({code: MtrDex.Blake3_256}, vcp)
+        let prefixer = new Prefixer({ code: MtrDex.Blake3_256 }, vcp)
         vcp.i = prefixer.qb64
         vcp.d = prefixer.qb64
 
@@ -492,7 +326,7 @@ export class Registries {
                 d: prefixer.qb64
             }]
 
-            let serder = interact({pre: pre, sn: sn + 1, data: data, dig: dig, version: undefined, kind: undefined})
+            let serder = interact({ pre: pre, sn: sn + 1, data: data, dig: dig, version: undefined, kind: undefined })
             let keeper = this.client!.manager!.get(hab)
             sigs = keeper.sign(b(serder.raw))
             ixn = serder.ked
