6dcf7726faf89a90cbcab8f2ea8ef1ef1d00a2c0
Rodolfo Miranda <rodolfo.miranda@gmail.com>
2024-07-29 12:39:03 -0300

    + tests

--- test/app/registry.test.ts
+++ test/app/registry.test.ts
@@ -106,4 +106,75 @@ describe('registry', () => {
             }
         );
     });
+
+    it('should create a registry when the anchor is already on the KEL', async () => {
+        await libsodium.ready;
+        const mockedClient = mock(SignifyClient);
+        const mockedIdentifiers = mock(Identifier);
+        const mockedKeyManager = mock(KeyManager);
+        const mockedKeeper = mock(SaltyKeeper);
+        const mockedKeyEvents = mock(KeyEvents);
+
+        const hab = {
+            prefix: 'hab prefix',
+            state: { i: 'a prefix', s: '1', d: 'a digest', p: 'old digest' } as State,
+        } as HabState;
+        const events = [
+            {
+                i: 'a prefix',
+                s: '0'
+            },
+            {
+                i: 'a prefix',
+                s: '1',
+                d: 'a 2nd digest',
+                a: [
+                    {
+                        i: 'EMppKX_JxXBuL_xE3A_a6lOcseYwaB7jAvZ0YFdgecXX',
+                        s: '0',
+                        d: 'EMppKX_JxXBuL_xE3A_a6lOcseYwaB7jAvZ0YFdgecXX',
+                }],
+            },
+        ];
+
+        when(mockedClient.manager).thenReturn(instance(mockedKeyManager));
+        when(mockedKeyManager.get(hab)).thenReturn(instance(mockedKeeper));
+
+        when(mockedKeeper.sign(anyOfClass(Uint8Array))).thenResolve([
+            'a signature',
+        ]);
+
+        when(mockedIdentifiers.get('a name')).thenResolve(hab);
+        when(mockedClient.identifiers()).thenReturn(
+            instance(mockedIdentifiers)
+        );
+        when(mockedKeyEvents.get(hab.prefix)).thenResolve(events);
+        when(mockedClient.keyEvents()).thenReturn(instance(mockedKeyEvents));
+
+        const mockedResponse = mock(Response);
+        when(
+            mockedClient.fetch(
+                '/identifiers/a name/registries',
+                'POST',
+                anything()
+            )
+        ).thenResolve(instance(mockedResponse));
+
+        const registries = new Registries(instance(mockedClient));
+
+        const actual = await registries.create({
+            name: 'a name',
+            registryName: 'a registry name',
+            nonce: '',
+        });
+
+        assert.equal(
+            actual.regser.raw,
+            '{"v":"KERI10JSON0000c5_","t":"vcp","d":"EMppKX_JxXBuL_xE3A_a6lOcseYwaB7jAvZ0YFdgecXX","i":"EMppKX_JxXBuL_xE3A_a6lOcseYwaB7jAvZ0YFdgecXX","ii":"hab prefix","s":"0","c":["NB"],"bt":"0","b":[],"n":""}'
+        );
+        assert.equal(
+            actual.serder.raw,
+            '{"v":"KERI10JSON0000f6_","t":"ixn","d":"EN9qu43BedCpLgKeeb01FfZa5T50mWNnpIdQEHF4k799","i":"hab prefix","s":"1","p":"old digest","a":[{"i":"EMppKX_JxXBuL_xE3A_a6lOcseYwaB7jAvZ0YFdgecXX","s":"0","d":"EMppKX_JxXBuL_xE3A_a6lOcseYwaB7jAvZ0YFdgecXX"}]}'
+        );
+    });
 });
