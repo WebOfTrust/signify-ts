ed842ab7f44f729ba106627bcaf155543ce24f2c
J Caso <jcaso@protonmail.com>
2024-11-06 19:57:36 +0100

    feat: wip essr exn message e2e

---
+++ examples/integration-scripts/exchanging.test.ts
@@ -0,0 +1,57 @@
+import signify, {SignifyClient} from "signify-ts";
+import {getOrCreateClient, getOrCreateIdentifier, waitOperation} from "./utils/test-util";
+import {resolveEnvironment} from "./utils/resolve-env";
+
+const { vleiServerUrl } = resolveEnvironment();
+const WITNESS_AIDS = [
+    'BBilc4-L3tFUnfM_wJr4S4OJanAv_VmF_dJNN6vkf2Ha',
+    'BLskRTInXnMxWaGqcpSyMgo0nYbalW99cGZESrz3zapM',
+    'BIKKuvBwpmDVA4Ds-EpL5bt9OqPzWPja2LigFYZN2YfX',
+];
+
+const SCHEMA_SAID = 'EBfdlu8R27Fbx-ehrqwImnK-8Cm79sqbAQ4MmvEAYqao';
+const SCHEMA_OOBI = `${vleiServerUrl}/oobi/${SCHEMA_SAID}`;
+
+test('ESSR exchanging', async function run() {
+    await signify.ready();
+    // Boot two clients
+    const [client1, client2] = await Promise.all([
+        getOrCreateClient(),
+        getOrCreateClient()
+    ]);
+
+    // Create two identifiers, one for each client
+    let [aid1, aid2, aid3, aid4] = await Promise.all([
+        createAID(client1, 'client1', WITNESS_AIDS),
+        createAID(client2, 'client2', WITNESS_AIDS),
+    ]);
+
+    // Exchange OOBIs
+    console.log('Resolving OOBIs');
+    const [oobi1, oobi2] = await Promise.all([
+        client1.oobis().get('client1', 'agent'),
+        client2.oobis().get('client2', 'agent'),
+    ]);
+
+    let op1 = await client1.oobis().resolve(oobi2.oobis[0], 'client2');
+    op1 = await waitOperation(client1, op1);
+    op1 = await client1.oobis().resolve(SCHEMA_OOBI, 'schema');
+    op1 = await waitOperation(client1, op1);
+    console.log('Client1 resolved client2 OOBI');
+
+
+    let op2 = await client2.oobis().resolve(oobi1.oobis[0], 'client1');
+    op2 = await waitOperation(client2, op2);
+    op2 = await client2.oobis().resolve(SCHEMA_OOBI, 'schema');
+    op2 = await waitOperation(client2, op2);
+    console.log('Client2 resolved client1 OOBI');
+
+});
+
+async function createAID(client: SignifyClient, name: string, wits: string[]) {
+    await getOrCreateIdentifier(client, name, {
+        wits: wits,
+        toad: wits.length,
+    });
+    return await client.identifiers().get(name);
+}
