cba180b3649bcd8b43f4d5bdedd9e148d974d4b8
iFergal <fergaloconnor1998@gmail.com>
2024-11-18 17:48:41 -0300

    prettier

--- examples/integration-scripts/test-setup-single-client.test.ts
+++ examples/integration-scripts/test-setup-single-client.test.ts
@@ -73,8 +73,8 @@ describe('test-setup-single-client', () => {
                     iurls: [
                         'http://127.0.0.1:5642/oobi/BBilc4-L3tFUnfM_wJr4S4OJanAv_VmF_dJNN6vkf2Ha/controller?name=Wan&tag=witness',
                         'http://127.0.0.1:5643/oobi/BLskRTInXnMxWaGqcpSyMgo0nYbalW99cGZESrz3zapM/controller?name=Wes&tag=witness',
-                        'http://127.0.0.1:5644/oobi/BIKKuvBwpmDVA4Ds-EpL5bt9OqPzWPja2LigFYZN2YfX/controller?name=Wil&tag=witness'
-                    ]
+                        'http://127.0.0.1:5644/oobi/BIKKuvBwpmDVA4Ds-EpL5bt9OqPzWPja2LigFYZN2YfX/controller?name=Wil&tag=witness',
+                    ],
                 });
                 break;
             case 'docker':
