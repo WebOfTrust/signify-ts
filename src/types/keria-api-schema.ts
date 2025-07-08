5d490a5915f14aa1ded252048e6b9fb291ab8b47
Patrick Vu <patrick.vu@sotatek.com>
2025-07-08 18:06:43 +0700

    demo generated types for credentials().get()

--- src/types/keria-api-schema.ts
+++ src/types/keria-api-schema.ts
@@ -2,6 +2,105 @@
 
 export interface components {
     schemas: {
+        SADAttributes: {
+            d: string;
+            i: string;
+            LEI: string;
+            dt: string;
+        };
+        SADSchema: {
+            v: string;
+            d: string;
+            i: string;
+            ri: string;
+            s: string;
+            a: components["schemas"]["SADAttributes"];
+            u?: boolean;
+            e?: boolean;
+            r?: boolean;
+        };
+        SADAttributesSchema: {
+            d: string;
+            i: string;
+            LEI: string;
+            dt: string;
+        };
+        ISSSchema: {
+            v: string;
+            t: string;
+            d: string;
+            i: string;
+            s: string;
+            ri: string;
+            dt: string;
+        };
+        SchemaSchema: {
+            $id: string;
+            $schema: string;
+            title: string;
+            description: string;
+            type: string;
+            credentialType: string;
+            version: string;
+            properties: {
+                [key: string]: unknown;
+            };
+            additionalProperties: boolean;
+            required: string[];
+        };
+        StatusAnchorSchema: {
+            s: number;
+            d: string;
+        };
+        CredentialStatusSchema: {
+            vn: number[];
+            i: string;
+            s: string;
+            d: string;
+            ri: string;
+            ra: {
+                [key: string]: unknown;
+            };
+            a: components["schemas"]["StatusAnchorSchema"];
+            dt: string;
+            et: string;
+        };
+        AnchorSchema: {
+            pre: string;
+            sn: number;
+            d: string;
+        };
+        SealSchema: {
+            i: string;
+            s: string;
+            d: string;
+            t?: boolean;
+            p?: boolean;
+        };
+        ANCSchema: {
+            v: string;
+            t: string;
+            d: string;
+            i: string;
+            s: string;
+            p: string;
+            a: components["schemas"]["SealSchema"][];
+        };
+        CredentialSchema: {
+            sad: components["schemas"]["SADSchema"];
+            atc: string;
+            iss: components["schemas"]["ISSSchema"];
+            issAtc: string;
+            pre: string;
+            schema: components["schemas"]["SchemaSchema"];
+            chains: {
+                [key: string]: unknown;
+            }[];
+            status: components["schemas"]["CredentialStatusSchema"];
+            anchor: components["schemas"]["AnchorSchema"];
+            anc: components["schemas"]["ANCSchema"];
+            ancAttachment: string;
+        };
         StatusSchema: {
             code: number;
             message: string;
